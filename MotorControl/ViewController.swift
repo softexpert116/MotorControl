a45aa3d822fe473c2831ba896876aa89041f73e8
ujs <ujs@ujss-mac.local>
2018-07-24 04:54:37 -0700

    Initial Commit

---
+++ MotorControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MotorControl
+//
+//  Created by ujs on 7/24/18.
+//  Copyright © 2018 ujs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
